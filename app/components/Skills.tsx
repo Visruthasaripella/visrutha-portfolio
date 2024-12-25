2d57a83d5fd5eb1092596874a240e2e653cf801f
visruthasaripella <visruthapurnima1@gmail.com>
2024-12-25 14:01:26 -0500

    Added section to skills

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaAws, FaDatabase, FaDocker } from "react-icons/fa";
-import {SiSpringboot, SiMongodb, SiPostman, SiGit, SiKubernetes, SiAngular, SiJquery, SiAstro, SiTailwindcss, SiTypescript, SiExpress, SiGradle, SiBitbucket, SiJenkins, SiBamboo, SiPostgresql } from "react-icons/si";
+import { FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaAws, FaDatabase, FaDocker, FaPython} from "react-icons/fa";
+import {SiSpringboot, SiMongodb, SiPostman, SiGit, SiKubernetes, SiAngular, SiJquery, SiAstro, SiTailwindcss, SiTypescript, SiExpress, SiGradle, SiBitbucket, SiJenkins, SiBamboo, SiPostgresql, SiPython, SiTensorflow, SiOpenai} from "react-icons/si";
+import {GiBrain} from "react-icons/gi";
 import "../Skills.css"; // Import a CSS file for styling
 
 const Skills = () => {
@@ -49,6 +50,17 @@ const Skills = () => {
             <SiGradle title="Gradle" />
             <SiJenkins title="Jenkins" />
             <SiBamboo title="Bamboo" />
+
+        {/* AI & ML */}
+            <div className="skills-box">
+              <h3>AI & ML</h3>
+              <div className="skills-icons">
+                <GiBrain title="Reinforcement Learning" /> {/* Example for a general AI/ML icon */}
+                <SiPython title="DQN" /> {/* Representing with Python for DQN */}
+                <SiTensorflow title="PPO" /> {/* Representing with TensorFlow for PPO */}
+                <FaPython title="PyTorch" /> {/* Representing PyTorch */}
+                <SiOpenai title="OpenAI Gym" /> {/* Representing OpenAI Gym */}
+
           </div>
         </div>
       </div>
