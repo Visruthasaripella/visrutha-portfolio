ec1ea18c427dfef72728b02d6a7050e233ddb7f0
visruthasaripella <visruthapurnima1@gmail.com>
2025-01-05 14:29:22 -0500

    added another page

--- app/components/Hero2.tsx
+++ app/components/Hero2.tsx
@@ -7,9 +7,10 @@ export default function Hero2() {
                     src="/Logo.png"
                     width={500}
                     height={600}
+                    alt="Logo for the software engineer portfolio"
                     className="mx-auto mb-8 border-4 border-white shadow-lg"
                 />
-                <h1 className="text-5xl font-bold mb-4 animate-fade-in-down">"Who Am I ?" - A Software Engineer for Today
+                <h1 className="text-5xl font-bold mb-4 animate-fade-in-down">&quot;Who Am I ?&quot; - A Software Engineer for Today
 
                 </h1>
                 <p className="text-2xl mb-8 animate-fade-in-up">In the contemporary digital era, I exemplify the evolving role of a software engineer who transcends conventional coding paradigms. By mastering Cloud Computing for scalable and resilient architectures, Artificial Intelligence and Machine Learning for crafting adaptive, data-driven solutions, and Full Stack Development for seamless integration of user interfaces and backend systems, I embody the indispensable competencies essential to innovate, adapt, and thrive in today's tech-driven world. Learning these critical skills is not just an advantage but a necessity, enabling me to solve complex challenges and contribute meaningfully to the relentless evolution of technology.</p>
