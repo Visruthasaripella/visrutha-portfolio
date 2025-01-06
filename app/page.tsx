a584d81c307dc15a5a737f88f0ecea920a83426d
visruthasaripella <visruthapurnima1@gmail.com>
2025-01-05 22:23:00 -0500

    added another page

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ import Experience from './components/Experience'
 import Achievements from './components/Achievements'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import Hero2 from "@/app/components/Hero2";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
       <Hero />
+      <Hero2 />
       <About />
       <Skills />
       <Projects />
